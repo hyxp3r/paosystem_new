d4dc49a80b012a0335741d6125607d620177afea
Evgeniy Ermakov <erma2001@mail.ru>
2022-12-27 23:18:25 +0700

    добалвена новая надпись

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.main, name = "main"),
-   
+    path("login", views.userAuth, name = "login"),
+    path("home", views.home, name = "home" ),
+    path("logout", views.user_logout, name = "logout"),
+    
 ] 
