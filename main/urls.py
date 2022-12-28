1f3cfcdb06d673e6c9732df8e351b644a42f35c3
Evgeniy Ermakov <erma2001@mail.ru>
2022-12-29 01:18:30 +0700

    Добавлены ссылки на приложение пао

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path("", views.main, name = "main"),
     path("login", views.userAuth, name = "login"),
     path("home", views.home, name = "home" ),
     path("logout", views.user_logout, name = "logout"),
+    path("pao/", include("pao.urls"))
     
 ] 
