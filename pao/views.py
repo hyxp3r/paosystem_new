6b820c0807072eb37bc0f08c39a8338bf23517e9
Evgeniy Ermakov <erma2001@mail.ru>
2022-12-30 01:23:29 +0700

    добавлена реализации бэкенда

--- pao/views.py
+++ pao/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import CheckEc
+from django.db.models import Sum
 # Create your views here.
 
 
@@ -12,6 +13,9 @@ from .models import CheckEc
 #@login_required(login_url = 'login')
 def contracts(request):
 
-    issues = CheckEc.objects.all().values()
-    print(issues)
-    return render(request, "pao/contracts.html")
+    issues = CheckEc.objects.all().select_related("contractName__expertEC__department")
+    sum_requests = CheckEc.objects.all().aggregate(Sum("verified"), Sum("declared"))
+    all_persent = round(sum_requests["verified__sum"]/sum_requests["declared__sum"]*100)
+ 
+   
+    return render(request, "pao/contracts.html", {"issues": issues, "sum_requests": sum_requests, "all_persent": all_persent})
