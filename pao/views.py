e29d6953aec6d2e37a03ee0293c97ff77c5e9b13
Ermakov <e.n.ermakov@nsuem.ru>
2022-12-29 15:50:16 +0700

    add test view for model

--- pao/views.py
+++ pao/views.py
@@ -4,10 +4,14 @@ from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .models import CheckEc
 # Create your views here.
 
 
 
 #@login_required(login_url = 'login')
 def contracts(request):
+
+    issues = CheckEc.objects.all().values()
+    print(issues)
     return render(request, "pao/contracts.html")
