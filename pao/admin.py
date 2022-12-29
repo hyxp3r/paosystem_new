271bfbac7234f0d3020f916cd02d01e6852c616a
Ermakov <e.n.ermakov@nsuem.ru>
2022-12-29 15:49:39 +0700

    add models to admin

--- pao/admin.py
+++ pao/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Department, Expert, ContractNames, CheckEc
 
 # Register your models here.
+admin.site.register(Department )
+admin.site.register(Expert )
+admin.site.register(ContractNames )
+admin.site.register(CheckEc )
