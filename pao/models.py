9c89c1ad54d7cf4efe0154cd5b3e0a475213e15f
Ermakov <e.n.ermakov@nsuem.ru>
2022-12-29 15:49:56 +0700

    create models to EC

--- pao/models.py
+++ pao/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Department(models.Model):
+
+    name = models.CharField(max_length = 100, verbose_name = "Название")
+    shortName = models.CharField(max_length=50, null=True, blank=True, verbose_name= "Сокращенное название")
+
+    class Meta:
+
+        verbose_name = 'Отдел'
+        verbose_name_plural = 'Отдел'
+        ordering = ('name',)
+    def __str__(self) -> str:
+
+        return (self.name)
+
+class Expert(models.Model):
+
+    fioExpert = models.CharField(max_length=50, verbose_name = "ФИО эксперта")
+    department = models.ForeignKey(Department,  null=True, blank=True, verbose_name = "Название отдела", on_delete=models.SET_NULL)
+
+
+    class Meta:
+
+        verbose_name = 'Эксперт'
+        verbose_name_plural = 'Эксперт'
+
+    def __str__(self) -> str:
+        return (self.fioExpert)
+
+    def calculateShortFio(self):
+
+        short_fio = self.fioExpert.split(' ')
+        short_fio = f'{short_fio[0]} {short_fio[1][0:1]}.{short_fio[2][0:1]}.'
+        return short_fio
+
+
+
+class ContractNames(models.Model):
+
+    sectionEC = models.CharField(max_length=10, verbose_name = "Номер пункта")
+    descriptionEC  = models.TextField(verbose_name = "Описание")
+    expertEC = models.ForeignKey(Expert, null=True, blank=True, verbose_name = "Эксперт", on_delete= models.SET_NULL)
+
+    class Meta:
+
+        verbose_name = 'Разделы ЭК'
+        verbose_name_plural = 'Разделы ЭК'
+    
+    def __str__(self) -> str:
+        return (self.sectionEC)
+
+
+class CheckEc(models.Model):
+
+    createdTime = models.DateTimeField(auto_now_add=True)
+    declared = models.IntegerField(verbose_name = 'Заявлено')
+    verified = models.IntegerField(verbose_name = 'Проверено')
+    contractName = models.ForeignKey(ContractNames, verbose_name="Номер пункта", on_delete = models.CASCADE)
+
+    class Meta:
+
+        verbose_name = 'Ход ЭК'
+        verbose_name_plural = 'Ход ЭК'
+
+    def __str__(self) -> str:
+        return (self.contractName.sectionEC)
