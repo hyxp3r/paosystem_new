f64b3caf2e38368c5cd43af6d41840e6f66f67a0
Evgeniy Ermakov <erma2001@mail.ru>
2023-01-03 16:13:02 +0700

    таблица с должностями добавлена в админку

--- pao/admin.py
+++ pao/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Department, Expert, ContractNames, CheckEc
+from .models import Department, Expert, ContractNames, CheckEc, Post
 
 # Register your models here.
 admin.site.register(Department )
 admin.site.register(Expert )
 admin.site.register(ContractNames )
 admin.site.register(CheckEc )
+admin.site.register(Post )
