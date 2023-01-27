3ee626eb1267b6f74c3dcc5b486f95de112c9744
Evgeniy Ermakov <erma2001@mail.ru>
2023-01-27 17:15:17 +0700

    удален лишний импорт

--- pao/urls.py
+++ pao/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ContractsListView, ConcatView, file, studyload, StudyLoadView
+from .views import ContractsListView, ConcatView, file, StudyLoadView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
