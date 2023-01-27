402f562aa12b657ba908ac839fdb6127e6458ed6
Evgeniy Ermakov <erma2001@mail.ru>
2023-01-27 17:08:06 +0700

    добавлен обработчик адреса для нагрузки

--- pao/urls.py
+++ pao/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ContractsListView, ConcatView, file
+from .views import ContractsListView, ConcatView, file, studyload, StudyLoadView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path("", login_required(ConcatView.as_view()), name = "mail"),
     path("pao/contracts", login_required(ContractsListView.as_view()), name = "paocontracts"),
-    path("pao/file1C", login_required(file), name = "file")
+    path("pao/file1C", login_required(file), name = "file"),
+    path("pao/sudyload", login_required(StudyLoadView.as_view()), name = "studyload" ),
     
 ] 
