c1e9c66a9e472ec0524ffcb00b7331cecefccf25
Ermakov <e.n.ermakov@nsuem.ru>
2023-04-24 14:59:53 +0700

    change task

--- oopk/tasks.py
+++ oopk/tasks.py
@@ -1,10 +1,15 @@
 from celery import shared_task
 from .reports import ReportOne
+from .xlsxResponse import ResponseXlsx
 
 @shared_task
 def make_report_one(request):
 
-    response = ReportOne().makeReport(request)
+    print(request)
+
+    data = ReportOne().get(request)
+    print(data)
+    response = ResponseXlsx(data).makeIO()
     return response
 
     
