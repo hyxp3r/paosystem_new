03ca7455957220b34b262d63208cbfa2ca337d15
Evgeniy Ermakov <erma2001@mail.ru>
2023-09-06 22:12:12 +0700

    Переход на другой домен

--- paosystem/settings.py
+++ paosystem/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["birzha.nsuem.ru", "127.0.0.1", "10.0.100.114", "0.0.0.0"]
+ALLOWED_HOSTS = ["digital-system.nsuem.ru", "127.0.0.1", "10.0.100.114", "0.0.0.0"]
 
 
 # Application definition
